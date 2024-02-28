f1a22ccdc7acfd417e0908967fd85e00c7ea0beb
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-02-28 09:38:28 -0600

    Thrid excercise

--- GO_BASES/excercise/2/main.go
+++ GO_BASES/excercise/2/main.go
@@ -1,8 +1,5 @@
 package main
 
 func main(){
-	//Explicit var declaration
-	var colorOne, colorTwo, colorThree string = "Green", "Black", "White"
-	// Other form of delcaration colorTwo:="Green"
-	println(colorOne,colorTwo,colorThree)
+	
 }
