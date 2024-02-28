6258e13132103d9cc8b96f82b08efc6881330e41
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-02-28 10:14:55 -0600

    Ex 4

--- GO_BASES/excercise/4/main.go
+++ GO_BASES/excercise/4/main.go
@@ -1,16 +1,22 @@
 package main
 
-
+import "fmt"
 func main(){
 	/*
-	   var lastName string = "Smith"
+	   var lastName string = "Smith" -- Correct
 
-		var age int = "35"
+		var age int = "35" -- Incorrect
 
-		boolean := "false"
+		boolean := "false" -- Incorrect
 
-		var salary string = 45857.90
+		var salary string = 45857.90 -- Incorrect
 
-		var firstName string = "Mary"
+		var firstName string = "Mary" -- Correct
 	*/
+	var lastName string = "Smith"
+	var age int = 35
+	boolean:= false
+	var salary float64 = 45857.90
+	var firstName string = "Mary"
+	fmt.Println(lastName,age,boolean,salary,firstName)
 }	
