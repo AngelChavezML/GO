4206164c8cf011f665c58ec52a706359ec261d43
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-02-28 09:37:14 -0600

    Second excercise

--- GO_BASES/excercise/2/main.go
+++ GO_BASES/excercise/2/main.go
@@ -3,6 +3,6 @@ package main
 func main(){
 	//Explicit var declaration
 	var colorOne, colorTwo, colorThree string = "Green", "Black", "White"
-
+	// Other form of delcaration colorTwo:="Green"
 	println(colorOne,colorTwo,colorThree)
 }
