f204e831e2091ebca325ce10e7728f98386fe6c0
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-02-28 10:09:15 -0600

    Ex 3

---
+++ GO_BASES/excercise/3/main.go
@@ -0,0 +1,25 @@
+package main
+
+
+func main(){
+	/*var 1firstName string -- Incorrect
+
+	var lastName string -- Correct
+
+	var int age -- Incorrect
+
+	1lastName := 6 -- Incorrect
+
+	var driver_license = true -- Incorrect
+
+	var person height int -- Incorrect
+
+	childsNumber := 2 -- Correct
+	var firstName string
+	var lastName string
+	var age int
+	lastName:= 6
+	var driverLicense = true
+	var personHeight int
+	*/
+}	
