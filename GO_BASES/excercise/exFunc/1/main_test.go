7dafe0482b9a0063572a79686731c3ef22e8a2ae
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-02-29 15:02:41 -0600

    Doing testing in point two

--- GO_BASES/excercise/exFunc/1/main_test.go
+++ GO_BASES/excercise/exFunc/1/main_test.go
@@ -8,5 +8,7 @@ import (
 
 func TestSalary(t *testing.T) {
 	//Test when salary when is smaller than 50000. Expected 0 and shows if it passed the test
-	assert.Equal(t, 0.0, discount(40000))
+	//assert.Equal(t, 0.0, discount(40000))
+	//Test when salary when is greater than 50000 and smaller than 150000. Expected 8670 and shows if it passed the test
+	assert.Equal(t, 8670.0, discount(51000))
 }
