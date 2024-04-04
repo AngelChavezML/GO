f7a3c80382da32bcbcc472ae4f8323007360505c
AngelChavezML <angel.chavezmorones@mercadolibre.com.mx>
2024-04-04 10:26:25 -0600

    feat: GetAll

--- SQL/e1/internal/products.go
+++ SQL/e1/internal/products.go
@@ -1,12 +1,14 @@
 package internal
 
+import "time"
+
 type Product struct {
 	Id           int
 	Name         string
 	Quantity     int
 	Code_value   string
 	Is_published bool
-	Expiration   string
+	Expiration   time.Time
 	Price        float64
 }
 type ProductRepository interface {
--- SQL/e1/internal/repository/products_sql.go
+++ SQL/e1/internal/repository/products_sql.go
@@ -1,1 +1,29 @@
 package repository
+
+import (
+	"database/sql"
+	"go/sql/internal"
+)
+
+type ProductsRepository struct {
+	DB *sql.DB
+}
+
+func (r *ProductsRepository) FindAll() (all_Products []internal.Product, err error) {
+	rows, err := r.DB.Query("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var product internal.Product
+		err := rows.Scan(&product.Id, &product.Name, &product.Quantity, &product.Code_value, &product.Is_published, &product.Expiration, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+		all_Products = append(all_Products, product)
+	}
+
+	return all_Products, nil
+}
